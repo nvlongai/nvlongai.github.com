c53b1e74617feffe84c52161618b307fe3a28f2c
nvlongai <nvlongai@gmail.com>
2026-03-20 15:36:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ async function generateLyrics() {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            //"Authorization": "Bearer YOUR_API_KEY"
+            "Authorization": "Bearer 60097700399,gen-lang-client-0230883437,Gemini API"
         },
         body: JSON.stringify({
             model: "gpt-4o-mini",
